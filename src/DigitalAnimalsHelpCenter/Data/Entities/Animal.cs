c6a45ac1fd45664a5079ca23f1cffa5eda8140b8
Thesadra <thesadra@yahoo.com>
2022-03-20 17:42:06 +0330

    initialize commit

---
+++ src/DigitalAnimalsHelpCenter/Data/Entities/Animal.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DigitalAnimalsHelpCenter.Data.Entities
+{
+    public class Animal
+    {
+        [Key]
+        public Guid GUID { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Name { get; set; }
+
+    }
+}
